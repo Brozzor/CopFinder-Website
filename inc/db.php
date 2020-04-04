5810f49de178c181bb3454dc92bb6c4a0f533018
Brozzor <377roro@gmail.com>
2020-04-05 01:21:49 +0200

    fix (bo) changement des identifiants de la base de donnée

--- inc/db.php
+++ inc/db.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:dbname=copbot;host=localhost', 'copbotapi', '6truUUSmamYZL1c1');
+$pdo = new PDO('mysql:dbname=copbot;host=localhost', 'copbot', 'FZJOKbh0tkfu73ZU');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
