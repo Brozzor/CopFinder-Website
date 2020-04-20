ba477ceb9353be8f261e8988478f344b0917d752
Brozzor <377roro@gmail.com>
2020-04-20 11:46:37 +0200

    fix (fo) mise a jour temporaire des privacy policy

--- privacy.php
+++ privacy.php
@@ -1,76 +1,153 @@
 <html class="">
 
 <head>
-    <meta charset="utf-8">
-    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta charset="utf-8" />
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
     <title>Privacy Policy CopFinder</title>
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons&amp;amp;subset=cyrillic" rel="stylesheet" type="text/css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" media="all" href="/css/copfinder.css " data-turbolinks-track="true">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons&amp;amp;subset=cyrillic"
+        rel="stylesheet" type="text/css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" media="all" href="/css/copfinder.css " data-turbolinks-track="true" />
 </head>
 
 <body class="signup-page">
     <nav class="navbar navbar-transparent navbar-absolute navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
-        <div class="container mt-5"><a class="ml-auto mr-auto top-logo" href="/" ><img width="350px" src="/img/logo/logo.png"></a></div>
+        <div class="container mt-5">
+            <a class="ml-auto mr-auto top-logo" href="/"><img width="350px" src="/img/logo/logo.png" /></a>
+        </div>
     </nav>
-    <div class="page-header header-filter" filter-color="black" style="background-image: url('/img/bg/policy-bg.jpg'); background-size: cover; background-position: top center;">
+    <div class="page-header header-filter" filter-color="black"
+        style="background-image: url('/img/bg/policy-bg.jpg'); background-size: cover; background-position: top center;">
         <div class="container">
             <div class="row">
                 <div class="col-md-10 ml-auto mr-auto">
                     <div class="card card-signup">
-                        <h2 class="card-title text-center mb-5">Protection des données personnelles</h2>
-                        <div class="card-body" style="padding: 0 50px 0 50px">
-                            
-                            <h4 class="mt-5">1.Le type d'informations collectées et les objectifs de la collecte.</h4>
-                            <p>Informations personnelles :</p>
-                            <ul>
-                                <li>Prénom, nom</li>
-                                <li>Adresse e-mail</li>
-                                <li>Adresse de livraison</li>
-                                <li>code postal</li>
-                                <li>Numéro de téléphone mobile</li>
-                                <li>Informations sur la carte bancaire</li>
-                            </ul>
-                            <p>Informations collectées automatiquement :</p>
-                            <ul>
-                                <li>Adresse IP</li>
-                                <li>Information sur le navigateur</li>
-                                <li>Information sur les cookies</li>
-                                <li>Informations sur le système d'exploitation.</li>
-                                <li>Informations sur les temps d'accès.</li>
-                                <li>Information sur la page web précédemment visités avant l'arriver sur cop-finder.com (référence)</li>
-                            </ul>
-                            <p>L'objectif de cette collecte d'informations personnelles est pour le bon fonctionnement du service ainsi que le remplissage automatique de formulaires.<br>L'objectif de la collecte automatique des informations est l'analyse statistique pour améliorer la qualité et la rapidité du service.</p>
-                            <h4 class="mt-5">2.Controle des informations personnelles.</h4>
-                            <p>Contrôle et modification des informations personnelles :</p>
-                            <ul>
-                                <li>Les informations personnelles peuvent être enregistrées ou modifiées dans l'onglet "Settings" dans l'extension chrome. </li>
-                                <li>Les informations collectées automatiquement ne sont pas disponibles pour l'utilisateur. </li>      
-                            </ul>
-                            <p>Stockage des données personnelles :</p>
-                            <ul>
-                                <li>Le stockage des informations personnelles est stocker sur l'ordinateur de l'utilisateur et il est aussi stocker dans notre base de données héberger sur un serveur loué appartenant à l'entreprise OVH</li>
-                                <li>Les données sur la carte bleu sont stockés sur votre ordinateur et n'est pas envoyer dans nos bases de données</li>
-                            </ul>
-                            <h4 class="mt-5">3.Protection des informations personnelles.</h4>
-                            <ul>
-                                <li>La protection des informations personnelles est mise en œuvre par la méthode HTTPS lors du transfert de votre ordinateur à nos serveurs. </li>
-                                <li>La base de données est protégée par les méthodes et les ressources de l'entreprise OVH : <a style="color: black;" href="https://www.ovh.com/fr/protection-donnees-personnelles/">https://www.ovh.com/fr/protection-donnees-personnelles/</a></li>
-                            </ul>
-                            <h4 class="mt-5">4. L'ordre de transfert des données personnelles à des tiers.</h4>
-                            <ul>
-                                <li>Nous ne transferons ni ne partageons aucunement vos données personnelles à des tiers.</li>
-                                <li>Les données peuvent être ouvertes dans les cas prévus par la loi applicable: </li>
-                                <ul>
-                                    <li>demande officielle des agences d'exécution</li>
-                                    <li>exécution d'un jugement</li>
-                                    <li>Fraude</li>
-                                    <li>protection des droits des utilisateurs</li>
-                                </ul>
-                            </ul>
-                            <h4 class="mt-5">5. Changement.</h4>
-                            <p>Cette page est suceptible d'etre mis à jour n'importe quand</p>
+                        <h2 class="card-title text-center mb-5">Privacy policy</h2>
+                        <div class="card-body" style="padding: 0 50px 0 50px;">
+                            <p>
+                                PRIVACY STATEMENT<br /><br />----<br /><br />SECTION 1 - WHAT DO WE DO WITH YOUR
+                                INFORMATION?<br /><br />When you purchase something from our store, as part of the
+                                buying and selling
+                                process, we collect the personal information you give us such as your name, address and
+                                email address.<br />When you browse our store, we also automatically receive your
+                                computer’s
+                                internet protocol (IP) address in order to provide us with information that helps us
+                                learn about your browser and operating system.<br />Email marketing (if applicable):
+                                With your
+                                permission, we may send you emails about our store, new products and other
+                                updates.<br /><br />SECTION 2 - CONSENT<br /><br />How do you get my consent?<br />When
+                                you provide us with
+                                personal information to complete a transaction, verify your credit card, place an order,
+                                arrange for a delivery or return a purchase, we imply that you consent to our collecting
+                                it
+                                and using it for that specific reason only.<br />If we ask for your personal information
+                                for a secondary reason, like marketing, we will either ask you directly for your
+                                expressed
+                                consent, or provide you with an opportunity to say no.<br /><br />How do I withdraw my
+                                consent?<br />If after you opt-in, you change your mind, you may withdraw your consent
+                                for us
+                                to contact you, for the continued collection, use or disclosure of your information, at
+                                anytime, by contacting us at contact@cop-finder.com or mailing us
+                                at:<br />copfinder<br /><br /><br />SECTION
+                                3 - DISCLOSURE<br /><br />We may disclose your personal information if we are required
+                                by law to do so or if you violate our Terms of Service.<br /><br />SECTION 4 -
+                                OVH and STRIPE<br /><br />Our
+                                store is hosted on OVH. They provide us with the online e-commerce platform that
+                                allows us to sell our products and services to you.<br />Your data is stored through
+                                OVH data storage, databases and the general OVH application. They store your
+                                data on a secure server behind a firewall.<br /><br />Payment:<br />If you choose a
+                                direct
+                                payment gateway to complete your purchase, then STRIPE stores your credit card data. It
+                                is encrypted through the Payment Card Industry Data Security Standard (PCI-DSS). Your
+                                purchase transaction data is stored only as long as is necessary to complete your
+                                purchase transaction. After that is complete, your purchase transaction information is
+                                deleted.<br />All
+                                direct payment gateways adhere to the standards set by PCI-DSS as managed by the PCI
+                                Security Standards Council, which is a joint effort of brands like Visa, Mastercard,
+                                American
+                                Express and Discover.<br />PCI-DSS requirements help ensure the secure handling of
+                                credit card information by our store and its service providers.<br />For more insight,
+                                you may also
+                                want to read OVH Terms of Service (https://us.ovhcloud.com/legal/terms-of-service) or Privacy
+                                Statement (https://us.ovhcloud.com/legal/privacy-policy) and STRIPE Terms of Service (https://stripe.com/fr/legal) or Privacy
+                                Statement (https://stripe.com/fr/privacy).<br /><br />SECTION 5 - THIRD-PARTY
+                                SERVICES<br /><br />In general, the third-party providers used by us will only collect,
+                                use and disclose your information to the extent necessary to allow them to perform the
+                                services they provide to us.<br />However, certain third-party service providers, such
+                                as payment gateways and other payment transaction processors, have their own privacy
+                                policies
+                                in respect to the information we are required to provide to them for your
+                                purchase-related transactions.<br />For these providers, we recommend that you read
+                                their privacy policies
+                                so you can understand the manner in which your personal information will be handled by
+                                these providers.<br />In particular, remember that certain providers may be located in
+                                or have
+                                facilities that are located a different jurisdiction than either you or us. So if you
+                                elect to proceed with a transaction that involves the services of a third-party service
+                                provider, then your information may become subject to the laws of the jurisdiction(s) in
+                                which that service provider or its facilities are located.<br />As an example, if you
+                                are
+                                located in Canada and your transaction is processed by a payment gateway located in the
+                                United States, then your personal information used in completing that transaction may be
+                                subject to disclosure under United States legislation, including the Patriot
+                                Act.<br />Once you leave our store’s website or are redirected to a third-party website
+                                or application,
+                                you are no longer governed by this Privacy Policy or our website’s Terms of
+                                Service.<br /><br />Links<br />When you click on links on our store, they may direct you
+                                away from our
+                                site. We are not responsible for the privacy practices of other sites and encourage you
+                                to read their privacy statements.<br /><br />SECTION 6 - SECURITY<br /><br />To protect
+                                your
+                                personal information, we take reasonable precautions and follow industry best practices
+                                to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or
+                                destroyed.<br />If you provide us with your credit card information, the information is
+                                encrypted using secure socket layer technology (SSL) and stored with a AES-256
+                                encryption.
+                                Although no method of transmission over the Internet or electronic storage is 100%
+                                secure, we follow all PCI-DSS requirements and implement additional generally accepted
+                                industry
+                                standards.<br /><br />SECTION 7 - COOKIES<br /><br />
+                                Our not use cookies. <br /><br /><br />SECTION 8 - AGE OF
+                                CONSENT<br /><br />
+                                By using this site, you represent that you are at least the age of majority in your
+                                state or province of residence, or that you are the age of majority in your state or
+                                province of
+                                residence and you have given us your consent to allow any of your minor dependents to
+                                use this site.<br /><br />SECTION 9 - CHANGES TO THIS PRIVACY POLICY<br /><br />We
+                                reserve the
+                                right to modify this privacy policy at any time, so please review it frequently. Changes
+                                and clarifications will take effect immediately upon their posting on the website. If we
+                                make
+                                material changes to this policy, we will notify you here that it has been updated, so
+                                that you are aware of what information we collect, how we use it, and under what
+                                circumstances,
+                                if any, we use and/or disclose it.<br />If our store is acquired or merged with another
+                                company, your information may be transferred to the new owners so that we may continue
+                                to sell
+                                products to you.
+                            </p>
+                            <p>&nbsp;</p>
+                            <p>SECTION 10 - CHROME EXTENSION &amp; AUTOCHECKOUT</p>
+                            <p>
+                                Our&nbsp;automatic&nbsp;purchasing&nbsp;software collects your payment and shipping
+                                information for the sole purpose of completing the checkout&nbsp;process without
+                                requiring
+                                additional user-input on Supreme's website. This data collection is not necessary for
+                                the software's operation as the last step of the purchase can be&nbsp;completed&nbsp;by
+                                the user
+                                manually.&nbsp;However, if this information&nbsp;is provided at the consent of the user,
+                                it is stored in an encrypted&nbsp;format on their local computer and is not exported to
+                                external servers or databases for storage.&nbsp;
+                            </p>
+                            <p>
+                                <br />QUESTIONS AND CONTACT INFORMATION<br /><br />If you would like to: access,
+                                correct, amend or delete any personal information we have about you, register a
+                                complaint, or simply
+                                want more information contact our Privacy Compliance Officer at contact@cop-finder.com or
+                                by mail at<br />copfinder<br />[Re: Privacy Compliance Officer]<br /><br />----
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -78,9 +155,13 @@
         </div>
         <footer class="footer">
             <div class="container">
-                <div class="copyright text-center">©&nbsp;<script type="text/javascript">
-                        document.write(new Date().getFullYear())
-                    </script>&nbsp; CopFinder</div>
+                <div class="copyright text-center">
+                    ©&nbsp;
+                    <script type="text/javascript">
+                        document.write(new Date().getFullYear());
+                    </script>
+                    &nbsp; CopFinder
+                </div>
             </div>
         </footer>
     </div>
