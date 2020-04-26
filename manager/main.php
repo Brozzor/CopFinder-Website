80865a08abc4c07bb69e943ad6c74f4a917a1b16
Brozzor <377roro@gmail.com>
2020-04-26 21:23:46 +0200

    fix (bo) modification de l'affichage de la clé publique

--- manager/main.php
+++ manager/main.php
@@ -133,7 +133,7 @@ if (isset($_GET['renew'])) {
     <script src="/js/jquery.min.js" type="text/javascript"></script>
     <script src="https://js.stripe.com/v3/"></script>
     <script type="text/javascript">
-        var stripe = Stripe('<?= $daysRemaining ?>');
+        var stripe = Stripe('<?php echo getSetting('stripe_pubKey') ?>');
 
         $('#buy').on('click', function(e) {
 
