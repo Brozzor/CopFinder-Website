e990ffad0839cc2e3bf0def75de9e8684b38d065
Brozzor <377roro@gmail.com>
2020-05-20 14:59:38 +0200

    fix (git) gitignore ne fonctionnais pas

--- inc/bdd.php
+++
@@ -1,2 +0,0 @@
-<?php
-$pdo = new PDO('mysql:dbname=copbot;host=127.0.0.1', 'copbotuser', 'mQ9Fohg6GI4V8fVi');
--- manager/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-setcookie('remember', NULL, -1);
-setcookie('remember', null, time() - 60 * 60 * 24 * 7, "/", null, false, true );
-unset($_SESSION['auth']);
-session_destroy();
-header('Location: index.php');
