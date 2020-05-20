0884a453503ab24d8a9be8e75646fd77fe066050
Brozzor <377roro@gmail.com>
2020-05-20 10:30:26 +0200

    feat (bo) modification du sql

--- inc/bdd.php
+++ inc/bdd.php
@@ -1,2 +1,2 @@
 <?php
-$pdo = new PDO('mysql:dbname=copbot;host=164.132.46.160', 'copbot', 'FZJOKbh0tkfu73ZU');
+$pdo = new PDO('mysql:dbname=copbot;host=127.0.0.1', 'copbotuser', 'mQ9Fohg6GI4V8fVi');
